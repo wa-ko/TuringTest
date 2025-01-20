72d40f2556ed073be1845a3297d890c9b42bfd80
wa-ko <wai.create2021@gmail.com>
2025-01-20 19:50:02 +0900

    fix

--- turingTest.py
+++ turingTest.py
@@ -215,7 +215,7 @@ elif st.session_state.page == 'chat':
 
                 # Calculate delay
                 char_count = len(full_response)
-                delay = 0.3 * char_count + random.gammavariate(1.5, 2.5)
+                delay = 0.7 * char_count + random.gammavariate(1.5, 3.5)
                 time.sleep(delay)
 
             except requests.exceptions.RequestException as e:
